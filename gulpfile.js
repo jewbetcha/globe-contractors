1eb4faf23ee0abed42bb2638b32e0900f3cc28c0
themezzilla <crollins199@gmail.com>
2016-06-21 11:21:43 -0500

    Got gulp working for minificationa and prefixing styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,23 @@
 'use strict';
 
 var gulp = require('gulp'),
+    rename = require('gulp-rename'),
+    cssmin = require('gulp-minify-css'),
+    autoprefixer = require('gulp-autoprefixer'),
     bb   = require('bitballoon');
 
 gulp.task('build', [], function() {
-  // Your build task
+  return gulp.src('./assets/css/*.css')
+    .pipe(autoprefixer({
+      browsers: ['last 3 versions'],
+      cascade: false
+    }))
+    .pipe(cssmin())
+    .pipe(gulp.dest('./assets/css/'));
+    //.pipe(rename('styles.min.css'));
 });
 
-gulp.task('deploy', ['build'], function() {
+gulp.task('deploy', function() {
   bb.deploy({
     access_token: "a35f74d49983f40f49bcc1134923dc31a652d10d78acfc1012ec2986539119b2",
     site_id: "http://veterinarian-bear-35244.bitballoon.com/",
