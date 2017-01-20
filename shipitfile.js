f5865e314ecb8941c5a3196398673fe5d3ddcc20
Coleman <crollins199@gmail.com>
2017-01-20 10:39:06 -0600

    [cr] working on getting shipit going for deployments

---
+++ shipitfile.js
@@ -0,0 +1,23 @@
+module.exports = function (shipit) {
+	require('shipit-deploy')(shipit);
+
+	shipit.initConfig({
+		default: {
+			workspace: '/tmp/globe-contractors',
+			deployTo: '/home/globeco',
+			dirToCopy: './build',
+			repositoryUrl: 'https://github.com/themezzilla/globe-contractors.git',
+			ignores: ['.git', 'node_modules'],
+			keepReleases: 2,
+			deleteOnRollback: false,
+			key: '~/.ssh/id_rsa.pub',
+			shallowClone: true
+
+		},
+		production: {
+			servers: 'globeco@globecontractors.com'
+		}
+
+	});
+
+};
