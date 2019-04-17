5bd1fc50bd732c5b4e654483d85a88b28bc25ddc
jewbetcha <crollins199@gmail.com>
2019-04-16 19:02:56 -0500

    [cr] correct key in shipitfile

--- shipitfile.js
+++ shipitfile.js
@@ -1,23 +1,21 @@
-module.exports = function (shipit) {
-	require('shipit-deploy')(shipit);
+module.exports = function(shipit) {
+    require("shipit-deploy")(shipit);
 
-	shipit.initConfig({
-		default: {
-			workspace: '/tmp/globe-contractors',
-			deployTo: '/home/globeco/deployments',
-			dirToCopy: './build',
-			repositoryUrl: 'https://github.com/themezzilla/globe-contractors.git',
-			ignores: ['.git', 'node_modules'],
-			keepReleases: 2,
-			deleteOnRollback: false,
-			key: '~/.ssh/id_rsa.pub',
-			shallowClone: true
-
-		},
-		production: {
-			servers: 'globeco@globecontractors.com'
-		}
-
-	});
-
+    shipit.initConfig({
+        default: {
+            workspace: "/tmp/globe-contractors",
+            deployTo: "/home/globeco/deployments",
+            dirToCopy: "./build",
+            repositoryUrl:
+                "https://github.com/themezzilla/globe-contractors.git",
+            ignores: [".git", "node_modules"],
+            keepReleases: 2,
+            deleteOnRollback: false,
+            key: "~/.ssh/id_rsa",
+            shallowClone: true
+        },
+        production: {
+            servers: "globeco@globecontractors.com"
+        }
+    });
 };
