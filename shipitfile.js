aec460b0f20e270f55683b52fd566aaa66278fad
jewbetcha <crollins199@gmail.com>
2019-05-13 18:19:30 -0500

    [cr] fix shipitfile for repo name

--- shipitfile.js
+++ shipitfile.js
@@ -1,21 +1,20 @@
 module.exports = function(shipit) {
-    require("shipit-deploy")(shipit);
+  require("shipit-deploy")(shipit);
 
-    shipit.initConfig({
-        default: {
-            workspace: "/tmp/globe-contractors",
-            deployTo: "/home/globeco/deployments",
-            dirToCopy: "./build",
-            repositoryUrl:
-                "https://github.com/themezzilla/globe-contractors.git",
-            ignores: [".git", "node_modules"],
-            keepReleases: 2,
-            deleteOnRollback: false,
-            key: "~/.ssh/id_rsa",
-            shallowClone: true
-        },
-        production: {
-            servers: "globeco@globecontractors.com"
-        }
-    });
+  shipit.initConfig({
+    default: {
+      workspace: "/tmp/globe-contractors",
+      deployTo: "/home/globeco/deployments",
+      dirToCopy: "./build",
+      repositoryUrl: "https://github.com/jewbetcha/globe-contractors.git",
+      ignores: [".git", "node_modules"],
+      keepReleases: 2,
+      deleteOnRollback: false,
+      key: "~/.ssh/id_rsa",
+      shallowClone: true
+    },
+    production: {
+      servers: "globeco@globecontractors.com"
+    }
+  });
 };
